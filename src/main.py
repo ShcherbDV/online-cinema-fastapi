922c170c8805b670a4098948f460cae677a154e8
Dmytro Shcherbyna <shcherbdmytro@gmail.com>
2025-12-17 20:34:05 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+from routes import accounts
+app = FastAPI(
+    title="Online Cinema API",
+    description="A simple API for management movies and users in online cinema",
+)
 
+api_version_prefix = "/api/v1"
 
-@app.get("/")
-async def read_root():
-    return {"message": "Hello, World!"}
+app.include_router(accounts.router, prefix=f"api_version_prefix/accounts", tags=["accounts"])
