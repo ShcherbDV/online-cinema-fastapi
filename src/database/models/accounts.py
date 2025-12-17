b7ed230dd39d50eed2caf2ea9e8e18359d9bf22d
Dmytro Shcherbyna <shcherbdmytro@gmail.com>
2025-12-17 18:34:04 +0100

    Add models to accounts.py: UserProfile, User, UserGroup

--- src/database/models/accounts.py
+++ src/database/models/accounts.py
@@ -1,4 +1,21 @@
+from datetime import datetime, timezone, date
 import enum
+from typing import List, Optional
+
+from sqlalchemy import (
+    Integer,
+    Enum,
+    String,
+    Boolean,
+    DateTime,
+    func,
+    ForeignKey,
+    Date,
+    Text, UniqueConstraint,
+)
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from src.database.models.base import Base
 
 
 class UserGroupEnum(str, enum.Enum):
@@ -10,3 +27,82 @@ class UserGroupEnum(str, enum.Enum):
 class GenderEnum(str, enum.Enum):
     MAN = "man"
     WOMAN = "woman"
+
+
+class UserGroupModel(Base):
+    __tablename__ = "user_groups"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    name: Mapped[UserGroupEnum] = mapped_column(
+        Enum(UserGroupEnum), nullable=False, unique=True
+    )
+    users: Mapped[List["UserModel"]] = relationship("UserModel", back_populates="users")
+
+    def __repr__(self):
+        return f"<UserGroupModel(id={self.id}, name={self.name})>"
+
+
+class UserModel(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    email: Mapped[str] = mapped_column(
+        String(255), unique=True, nullable=False, index=True
+    )
+    _hashed_password: Mapped[str] = mapped_column(
+        "hashed_password", String(255), nullable=False
+    )
+    is_active: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now(),
+        nullable=False,
+    )
+    group_id: Mapped[int] = mapped_column(
+        ForeignKey("user_groups.id", ondelete="CASCADE"), nullable=False
+    )
+    group: Mapped["UserGroupModel"] = relationship(
+        "UserGroupModel", back_populates="users"
+    )
+
+    profile: Mapped[Optional["UserProfileModel"]] = relationship(
+        "UserProfileModel", back_populates="user", cascade="all, delete-orphan"
+    )
+    activation_token: Mapped[Optional["ActivationTokenModel"]] = relationship(
+        "ActivationTokenModel", back_populates="user", cascade="all, delete-orphan"
+    )
+    password_reset_token: Mapped[Optional["PasswordResetTokenModel"]] = relationship(
+        "PasswordResetTokenModel", back_populates="user", cascade="all, delete-orphan"
+    )
+    refresh_token: Mapped[List["RefreshTokenModel"]] = relationship(
+        "RefreshTokenModel", back_populates="user", cascade="all, delete-orphan"
+    )
+
+    def __repr__(self):
+        return (
+            f"<UserModel(id={self.id}, email={self.email}, is_active={self.is_active})>"
+        )
+
+
+class UserProfileModel:
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id", ondelete="CASCADE"), nullable=False, unique=True
+    )
+    first_name: Mapped[Optional[str]] = mapped_column(String(100))
+    last_name: Mapped[Optional[str]] = mapped_column(String(100))
+    avatar: Mapped[Optional[str]] = mapped_column(String(255))
+    gender: Mapped[Optional[GenderEnum]] = mapped_column(Enum(GenderEnum))
+    date_of_birth: Mapped[Optional[date]] = mapped_column(Date)
+    info: Mapped[Optional[str]] = mapped_column(Text)
+    user: Mapped[UserModel] = relationship("UserModel", back_populates="profile")
+
+    __table_args__ = (UniqueConstraint("user_id"),)
+
+    def __repr__(self):
+        return (f"<UserProfile(id={self.id}, first_name={self.first_name}, last_name={self.last_name}), "
+                f"gender={self.gender}, date_of_birth={self.date_of_birth}, info={self.info})>")
