5ef15c4d3dabb3ee150aaa27de3e14708083888d
Dmytro Shcherbyna <shcherbdmytro@gmail.com>
2025-12-18 18:24:49 +0100

    Create settings.py with base app settings for postgresql connection

---
+++ src/config/settings.py
@@ -0,0 +1,23 @@
+import os
+from pathlib import Path
+
+from pydantic_settings import BaseSettings
+
+
+class BaseAppSettings(BaseSettings):
+    BASE_DIR: Path = Path(__file__).parent.parent
+    PATH_TO_DB: str = str(BASE_DIR / "database" / "source" / "cinema.db")
+
+    LOGIN_TIME_DAYS = 7
+
+
+class Settings(BaseAppSettings):
+    POSTGRES_USER: str = os.getenv("POSTGRES_USER", "my_user")
+    POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD", "my_password")
+    POSTGRES_HOST: str = os.getenv("POSTGRES_HOST", "my_host")
+    POSTGRES_DB_PORT: int = int(os.getenv("POSTGRES_DB_PORT", 5432))
+    POSTGRES_DB: str = os.getenv("POSTGRES_DB", "cinema_db")
+
+    SECRET_KEY_ACCESS: str = os.getenv("SECRET_KEY_ACCESS", os.urandom(32))
+    SECRET_KEY_REFRESH: str = os.getenv("SECRET_KEY_REFRESH", os.urandom(32))
+    JWT_SIGNING_ALGORITHM: str = os.getenv("JWT_SIGNING_ALGORITHM", "HS256")
