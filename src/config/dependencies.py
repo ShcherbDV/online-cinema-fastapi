6ef039ce1641f1dbbe3b4aeb8fd5f590ffc45a25
Dmytro Shcherbyna <shcherbdmytro@gmail.com>
2025-12-25 16:18:12 +0100

    update dependencies.py -> add get_jwt_auth_manager func

--- src/config/dependencies.py
+++ src/config/dependencies.py
@@ -5,6 +5,8 @@ from fastapi import Depends
 from src.config.settings import BaseAppSettings, Settings, TestingSettings
 from src.notifications.emails import EmailSender
 from src.notifications.interfaces import EmailSenderInterface
+from src.security.interfaces import JWTAuthManagerInterface
+from src.security.token_manager import JWTAuthManager
 
 
 def get_settings() -> BaseAppSettings:
@@ -52,4 +54,26 @@ def get_accounts_email_notificator(
         activation_complete_email_template_name=settings.ACTIVATION_COMPLETE_EMAIL_TEMPLATE_NAME,
         password_email_template_name=settings.PASSWORD_RESET_TEMPLATE_NAME,
         password_complete_email_template_name=settings.PASSWORD_RESET_COMPLETE_TEMPLATE_NAME,
+    )
+
+def get_jwt_auth_manager(settings: BaseAppSettings = Depends(get_settings)) -> JWTAuthManagerInterface:
+    """
+    Create and return a JWT authentication manager instance.
+
+    This function uses the provided application settings to instantiate a JWTAuthManager, which implements
+    the JWTAuthManagerInterface. The manager is configured with secret keys for access and refresh tokens
+    as well as the JWT signing algorithm specified in the settings.
+
+    Args:
+        settings (BaseAppSettings, optional): The application settings instance.
+        Defaults to the output of get_settings().
+
+    Returns:
+        JWTAuthManagerInterface: An instance of JWTAuthManager configured with
+        the appropriate secret keys and algorithm.
+    """
+    return JWTAuthManager(
+        secret_key_access=settings.SECRET_KEY_ACCESS,
+        secret_key_refresh=settings.SECRET_KEY_REFRESH,
+        algorithm=settings.JWT_SIGNING_ALGORITHM
     )
