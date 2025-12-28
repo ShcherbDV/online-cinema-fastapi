a942681da6e45adb2b81194ed5d8402d7ce3f884
Dmytro Shcherbyna <shcherbdmytro@gmail.com>
2025-12-28 21:46:34 +0100

    create schemas/ratings.py -> schemas for routs for ratings

---
+++ src/schemas/ratings.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, Field
+
+class MovieRatingCreateSchema(BaseModel):
+    rating: int = Field(..., ge=0, le=10)
+
+
+class MovieRatingResponseSchema(BaseModel):
+    movie_id: int
+    rating: int
+
+
+class MessageResponseSchema(BaseModel):
+    message: str
