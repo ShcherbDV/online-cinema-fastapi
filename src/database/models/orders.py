fe7ccf3f55d1855b4678db4acf582c9a40751ed2
Dmytro Shcherbyna <shcherbdmytro@gmail.com>
2025-12-28 21:43:55 +0100

    update orders.py -> fix relationship

--- src/database/models/orders.py
+++ src/database/models/orders.py
@@ -10,7 +10,6 @@ class OrderItemModel(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
     movie_id: Mapped[int] = mapped_column(ForeignKey("movies.id"), nullable=False)
-    user: Mapped["UserModel"] = relationship(
-        "CertificationModel", back_populates="order_items"
-    )
+
     movie: Mapped["MovieModel"] = relationship("MovieModel", back_populates="order_items")
+    user: Mapped["UserModel"] = relationship("UserModel", back_populates="order_items")
