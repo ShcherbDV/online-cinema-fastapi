0b6712e871a2dc2f7f80fe82c8a24293082c9c96
Dmytro Shcherbyna <shcherbdmytro@gmail.com>
2025-12-28 21:45:58 +0100

    create models/ratings.py -> for saving rates of users for movies

---
+++ src/database/models/ratings.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Integer, ForeignKey, UniqueConstraint, CheckConstraint
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from database.models.base import Base
+
+
+class MovieRatingModel(Base):
+    __tablename__ = "movie_ratings"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    movie_id: Mapped[int] = mapped_column(ForeignKey("movies.id", ondelete="CASCADE"), nullable=False)
+
+    movie: Mapped["MovieModel"] = relationship("MovieModel", back_populates="movie_ratings")
+    user: Mapped["UserModel"] = relationship("UserModel", back_populates="ratings")
+
+    __table_args__ = (
+        UniqueConstraint("user_id", "movie_id", name="uq_user_movie_rating"),
+        CheckConstraint("rating >= 0 AND rating <= 10", name="ck_rating_range"),
+    )
