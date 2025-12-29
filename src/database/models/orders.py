6adc25d19cfcf1bbeada5e7c3d3c4f47354e01af
Dmytro Shcherbyna <shcherbdmytro@gmail.com>
2025-12-29 10:46:38 +0100

    update __init__.py

--- src/database/models/orders.py
+++ src/database/models/orders.py
@@ -7,7 +7,7 @@ from database.models.base import Base
 class OrderItemModel(Base):
     __tablename__ = "order_items"
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
     movie_id: Mapped[int] = mapped_column(ForeignKey("movies.id"), nullable=False)
 
