1adae596a3415c35b64a0984d918e6d24d631463
Dmytro Shcherbyna <shcherbdmytro@gmail.com>
2025-12-29 19:12:45 +0100

    update routes/favorites.py -> edit route for catalog -> get_favorites add search, sort...

--- src/routes/movies/favorites.py
+++ src/routes/movies/favorites.py
@@ -1,12 +1,14 @@
 from fastapi import APIRouter, status, HTTPException
-from fastapi.params import Depends
-from sqlalchemy import select
+from fastapi.params import Depends, Query
+from sqlalchemy import select, func
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database.models import UserModel, MovieModel, FavoriteMovieModel
 from config.dependencies import get_current_user
 from database import get_db
 
+from schemas.movies import MovieListResponseSchema, MovieCatalogParams, MovieListItemSchema
+from services.movie.catalog import build_movie_catalog_query
 
 router = APIRouter(prefix="/favorites", tags=["Favorites"])
 
@@ -73,3 +75,56 @@ async def delete_from_favorites(
     await db.delete(favorite)
     await db.commit()
     return {"detail": "Movie deleted from favorites"}
+
+
+@router.get("/favorites",
+            summary="List of favorites movies user",
+            description="List of favorites movies user",
+            response_model=MovieListResponseSchema,
+            status_code=status.HTTP_200_OK,
+)
+async def get_favorites(
+        page: int = Query(1, ge=1, description="Page number (1-based index)"),
+        per_page: int = Query(10, ge=1, le=20, description="Number of items per page"),
+        params: MovieCatalogParams = Depends(),
+        db: AsyncSession = Depends(get_db),
+        user: UserModel = Depends(get_current_user),
+):
+
+    offset = (page - 1) * per_page
+
+    count_stmt = select(func.count(MovieModel.id))
+    result_count = await db.execute(count_stmt)
+    total_items = result_count.scalar() or 0
+
+    if not total_items:
+        raise HTTPException(status_code=404, detail="No movies found.")
+
+    stmt = build_movie_catalog_query(params, only_favorites=True, user_id=user.id)
+
+    stmt = stmt.offset(offset).limit(per_page)
+
+    result_movies = await db.execute(stmt)
+    movies = result_movies.scalars().all()
+
+    if not movies:
+        raise HTTPException(status_code=404, detail="No movies found.")
+
+    movie_list = [MovieListItemSchema.model_validate(movie) for movie in movies]
+
+    total_pages = (total_items + per_page - 1) // per_page
+
+    response = MovieListResponseSchema(
+        movies=movie_list,
+        prev_page=(
+            f"/cinema/favorites/?page={page - 1}&per_page={per_page}" if page > 1 else None
+        ),
+        next_page=(
+            f"/cinema/favorites/?page={page + 1}&per_page={per_page}"
+            if page < total_pages
+            else None
+        ),
+        total_pages=total_pages,
+        total_items=total_items,
+    )
+    return response
