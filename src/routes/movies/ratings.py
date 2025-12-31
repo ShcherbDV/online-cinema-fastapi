cce445d2552791e78d2104ac8a1844706b15a5cc
Dmytro Shcherbyna <shcherbdmytro@gmail.com>
2025-12-31 19:05:15 +0100

    add descriptions for ratings.py routers

--- src/routes/movies/ratings.py
+++ src/routes/movies/ratings.py
@@ -25,6 +25,26 @@ async def rate_movie(
     db: AsyncSession = Depends(get_db),
     user: UserModel = Depends(get_current_user)
 ):
+    """
+    Add a rate to the database to a movie by user.
+
+    This endpoint allows the adding rate to a movie from a user.
+
+    :param movie_id: The ID of the movie to rate for.
+    :type movie_id: int
+    :param data: The data required to create a rate.
+    :type data: MovieRatingCreateSchema
+    :param db: The SQLAlchemy async database session (provided via dependency injection).
+    :type db: AsyncSession
+    :param user: The SQLAlchemy user model (provided via dependency injection).
+    :type user: UserModel
+
+    :return: The created a rate for movie by user.
+    :rtype: MessageResponseSchema
+
+    :raises HTTPException:
+        - 404 if the movie with the given ID is not found.
+    """
     movie = await db.get(MovieModel, movie_id)
 
     if not movie:
@@ -62,6 +82,24 @@ async def remove_rating(
         db: AsyncSession = Depends(get_db),
         user: UserModel = Depends(get_current_user)
 ):
+    """
+    Delete a rate for a specific movie by its ID.
+
+    This function deletes a rate for movie identified by its unique ID.
+    If the rate does not exist, a 404 error is raised.
+
+    :param movie_id: The unique identifier of the rate to delete.
+    :type movie_id: int
+    :param db: The SQLAlchemy database session (provided via dependency injection).
+    :type db: AsyncSession
+    :param user: The SQLAlchemy user model (provided via dependency injection).
+    :type user: UserModel
+
+    :raises HTTPException: Raises a 404 error if the genre with the given ID is not found.
+
+    :return: A response indicating the successful deletion of the rate.
+    :rtype: MessageResponseSchema
+    """
     stmt = select(MovieRatingModel).where(
         MovieRatingModel.user_id == user.id,
         MovieRatingModel.movie_id == movie_id,
