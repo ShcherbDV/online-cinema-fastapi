e5bd0fd963547e246bd68bea8e7b384c745f560e
Dmytro Shcherbyna <shcherbdmytro@gmail.com>
2026-01-03 16:49:42 +0100

    update __init__.py -> delete imports

--- src/database/models/__init__.py
+++ src/database/models/__init__.py
@@ -1,6 +1,1 @@
-from accounts import UserModel
-from movies import MovieModel
-from orders import OrderItemModel
-from ratings import MovieRatingModel
-from comments import MovieCommentModel
-from favorites import FavoriteMovieModel
+
