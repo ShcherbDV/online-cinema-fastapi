662aeeee0179b8cc5b4d72668bf197d5e7f067b8
Dmytro Shcherbyna <shcherbdmytro@gmail.com>
2026-01-03 16:30:06 +0100

    fix imports

--- src/database/session_sync.py
+++ src/database/session_sync.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from config.dependencies import get_settings
+from config.settings import get_settings
 
 settings = get_settings()
 
